bc38b0d0b5a51a48210e9e4babec901c7362eb85
Xvezda <xvezda@naver.com>
2022-12-26 18:36:54 +0900

    Update dmg url

--- Casks/teamup.rb
+++ Casks/teamup.rb
@@ -4,12 +4,12 @@ cask "teamup" do
   version "4.0.6.29"
   sha256 intel: "674fe14d9973bae6e5b115f42cfa558f2fd8e69ed0514c51d1f5bbcc90cd8b7d"
 
-  url "https://advert.estsoft.com/?event=201702222151584"
+  url "https://download.tmup.com/setup/app/20220921/TeamUPSetup#{version}.dmg"
   name "TeamUP"
   homepage "https://tmup.com/main/download"
 
   livecheck do
-    url :url
+    url "https://advert.estsoft.com/?event=201702222151584"
     strategy :header_match
     regex(/TeamUPSetup(\d+(?:\.\d+)*)\.dmg/i)
   end
